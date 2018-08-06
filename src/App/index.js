dbae999f6d3d69c53edb83759cbae9d3c7bc09c5
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-06 11:04:46 +0700

    Migrate to use react-app-rewired for hot-reload

--- src/App/index.js
+++ src/App/index.js
@@ -1,22 +1,17 @@
-import React, { Component } from "react";
+import React from 'react';
 
-import logo from "./logo.svg";
-import "./index.css";
+import logo from './logo.png';
+import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">MyTasks</h1>
+      </header>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// Add this import:
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.hydrate(<App />, document.getElementById("root"));
+// Wrap the rendering in a function:
+const render = Component => {
+  ReactDOM.hydrate(
+    // Wrap App inside AppContainer
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
 
+// Do this once
 registerServiceWorker();
+
+// Render once
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
