21043d8fa397676028fbdf9410268d6c66987c84
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:08:40 +0700

    Style Title

--- src/Title/index.js
+++ src/Title/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Title = () => <div>Title</div>
+const TitleStyled = styled.h1`
+  font-family: 'Roboto Slab', serif;
+  font-weight: 700;
+  margin: 30px 0;
+`
+
+const Title = ({ value }) => <TitleStyled>{value}</TitleStyled>
+
+Title.propTypes = {
+  value: PropTypes.string.isRequired
+}
 
 export default Title
