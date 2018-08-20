549256a1ef21fcb3cc79f9266c6b247eb793c8fb
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:08:30 +0700

    Header contains Title, DateTime

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Header = () => <div>Header</div>
+import Title from '../Title'
+import DateTime from '../DateTime'
+
+const Header = () => (
+  <div>
+    <Title value="MyTasks" />
+    <DateTime value="Monday, 20 August 2018 11:32" />
+  </div>
+)
 
 export default Header
