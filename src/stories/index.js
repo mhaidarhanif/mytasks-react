ae2fa897d42086d5e22ef5b093eafda89316af9d
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:09:03 +0700

    Add Title & DateTime stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,6 +5,8 @@ import App from '../App'
 import Page from '../Page'
 import Cover from '../Cover'
 import Header from '../Header'
+import Title from '../Title'
+import DateTime from '../DateTime'
 import Todos from '../Todos'
 import Footer from '../Footer'
 
@@ -12,5 +14,11 @@ storiesOf('App', module).add('Default', () => <App>App</App>)
 storiesOf('Page', module).add('Default', () => <Page>Page</Page>)
 storiesOf('Cover', module).add('Default', () => <Cover>Cover</Cover>)
 storiesOf('Header', module).add('Default', () => <Header>Header</Header>)
+storiesOf('Title', module).add('Default', () => (
+  <Title value="MyTasks">Title</Title>
+))
+storiesOf('DateTime', module).add('Default', () => (
+  <DateTime value="Monday, 20 August 2018 11:32">DateTime</DateTime>
+))
 storiesOf('Todos', module).add('Default', () => <Todos>Todos</Todos>)
 storiesOf('Footer', module).add('Default', () => <Footer>Footer</Footer>)
