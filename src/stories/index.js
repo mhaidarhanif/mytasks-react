c48c70130eb9170a659cc4ee5fd074506141399f
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:19:42 +0700

    Refine stories of Cover, Header, etc

--- src/stories/index.js
+++ src/stories/index.js
@@ -10,10 +10,19 @@ import DateTime from '../DateTime'
 import Todos from '../Todos'
 import Footer from '../Footer'
 
-storiesOf('App', module).add('Default', () => <App>App</App>)
-storiesOf('Page', module).add('Default', () => <Page>Page</Page>)
-storiesOf('Cover', module).add('Default', () => <Cover>Cover</Cover>)
-storiesOf('Header', module).add('Default', () => <Header>Header</Header>)
+storiesOf('App', module).add('Default', () => <App />)
+storiesOf('Page', module).add('Default', () => <Page />)
+storiesOf('Cover', module).add('Default', () => (
+  <Cover>
+    <Header />
+  </Cover>
+))
+storiesOf('Header', module).add('Default', () => (
+  <Header>
+    <Title value="MyTasks">Title</Title>
+    <DateTime value="Monday, 20 August 2018 11:32">DateTime</DateTime>
+  </Header>
+))
 storiesOf('Title', module).add('Default', () => (
   <Title value="MyTasks">Title</Title>
 ))
