163ff4728f029a5ba839aa10bd50238d6e8f4326
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:44:09 +0700

    Style DatTime and use dayjs

--- src/DateTime/index.js
+++ src/DateTime/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import dayjs from 'dayjs'
 
-const DateTime = ({ value }) => <div>{value}</div>
+const DateTimeStyled = styled.h2`
+  font-size: 1em;
+  font-weight: 400;
+  margin: 10px 0;
+`
+
+const DateTime = ({ value = new Date() }) => {
+  const valueFormatted = dayjs(value).format('dddd, D MMMM YYYY H:mm')
+
+  return <DateTimeStyled>{valueFormatted}</DateTimeStyled>
+}
 
 DateTime.propTypes = {
   value: PropTypes.string.isRequired
