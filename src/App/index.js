cb9a63a8852c480886550b9442d2d6366854ad0f
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-21 02:35:57 +0700

    Use Router, Route, Switch

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Page from '../Page'
 import PageHome from '../PageHome'
 import PageAccount from '../PageAccount'
+import PageNotFound from '../PageNotFound'
 
 import './index.css'
 
 const App = () => (
-  <Page>
-    <PageHome />
-    <PageAccount />
-  </Page>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={PageHome} />
+      <Route path="/account" component={PageAccount} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </Router>
 )
 
 export default App
