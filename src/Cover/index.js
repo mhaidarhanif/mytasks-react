e57ec507d36b7a2e9818d3d09c9604842a4902af
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:43:56 +0700

    Style Cover with image

--- src/Cover/index.js
+++ src/Cover/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Header from '../Header'
 
+import IMAGE_COVER from './assets/cover.jpg'
+
+const CoverStyled = styled.div`
+  background: #eee;
+  background-image: url('${IMAGE_COVER}');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center top;
+  min-height: 100px;
+  min-width: 100px;
+`
+
 const Cover = () => (
-  <div>
+  <CoverStyled>
     <Header />
-  </div>
+  </CoverStyled>
 )
 
 export default Cover
