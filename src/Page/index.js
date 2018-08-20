580b3cc586e1eae0b69316216dc64e80d2888d4b
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 12:19:57 +0700

    Style Page with styled-components

--- src/Page/index.js
+++ src/Page/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Page = ({ children }) => <div>{children}</div>
+const PageStyled = styled.main`
+  border: 4px solid #333;
+  min-height: 360px;
+  min-width: 100px;
+`
+
+const Page = ({ children }) => <PageStyled>{children}</PageStyled>
 
 Page.propTypes = {
   children: PropTypes.array.isRequired
