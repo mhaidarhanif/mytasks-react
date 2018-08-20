6c42dec989a9c5152ca0d0841854f0d74aff74ad
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-20 22:24:29 +0700

    Implement FormTaskAdd handle change and submit

---
+++ src/FormTaskAdd/FormTaskAddStyled/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Form = styled.form`
+  padding: 20px;
+  margin: 0;
+  color: #ccc;
+`
+
+const Input = styled.input`
+  padding: 8px 15px;
+  margin: 0;
+  transition: 200ms;
+
+  &::selection {
+    background: hsla(190, 100%, 50%, 0.2);
+  }
+
+  &::placeholder {
+    color: hsla(190, 100%, 50%, 1);
+    transition: 200ms;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus::placeholder {
+    color: #ccc;
+  }
+
+  &:focus .action.add::after {
+    content: url('/images/plus-gray.svg');
+  }
+`
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  border-radius: 20px;
+  font-size: 20px;
+  float: left;
+
+  &::after {
+    content: url('/images/plus.svg');
+    font-weight: 900;
+    color: hsla(190, 100%, 50%, 1);
+    transition: 200ms;
+  }
+`
+
+const FormTaskAddStyled = ({ onSubmit, onChange }) => (
+  <Form autocomplete="off" onSubmit={onSubmit}>
+    <Input
+      placeholder="What do you want to do?"
+      type="text"
+      autoFocus
+      onChange={onChange}
+    />
+    <Button
+      title="Action to do"
+      type="submit"
+      role="checkbox"
+      aria-checked="false"
+    />
+  </Form>
+)
+
+FormTaskAddStyled.propTypes = {
+  onSubmit: PropTypes.func,
+  onChange: PropTypes.func
+}
+
+export default FormTaskAddStyled
--- src/FormTaskAdd/index.js
+++ src/FormTaskAdd/index.js
@@ -1,78 +1,40 @@
 import React from 'react'
-import styled from 'styled-components'
 
-const FormTaskAddStyled = styled.form`
-  padding: 20px;
-  margin: 0;
-  color: #ccc;
-`
+import FormTaskAddStyled from './FormTaskAddStyled'
 
-const InputTaskAddStyled = styled.input`
-  padding: 8px 15px;
-  margin: 0;
-  transition: 200ms;
+class FormTaskAdd extends React.Component {
+  constructor(props) {
+    super(props)
 
-  &::selection {
-    background: hsla(190, 100%, 50%, 0.2);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.state = {
+      input: ''
+    }
   }
 
-  &::placeholder {
-    color: hsla(190, 100%, 50%, 1);
-    transition: 200ms;
+  handleChange(event) {
+    const input = event.target.value
+    this.setState({
+      input
+    })
   }
 
-  &:focus {
-    outline: none;
+  handleSubmit(event) {
+    event.preventDefault()
+
+    // console.log({ input: this.state.input })
   }
 
-  &:focus::placeholder {
-    color: #ccc;
+  render() {
+    return (
+      <FormTaskAddStyled
+        onChange={this.handleChange}
+        onSubmit={this.handleSubmit}
+      />
+    )
   }
-
-  &:focus .action.add::after {
-    content: url('/images/plus-gray.svg');
-  }
-`
-
-const ButtonTaskAddStyled = styled.button`
-  background: none;
-  border: none;
-  border-radius: 20px;
-  font-size: 20px;
-  float: left;
-
-  &::after {
-    content: url('/images/plus.svg');
-    font-weight: 900;
-    color: hsla(190, 100%, 50%, 1);
-    transition: 200ms;
-  }
-`
-
-const handleSubmit = event => {
-  event.preventDefault()
 }
-
-const FormTaskAdd = () => (
-  <FormTaskAddStyled
-    autocomplete="off"
-    className="segment add"
-    onSubmit={handleSubmit}
-  >
-    <InputTaskAddStyled
-      className="insert add"
-      type="text"
-      placeholder="What do you want to do?"
-      autoFocus
-    />
-    <ButtonTaskAddStyled
-      className="action add"
-      type="submit"
-      title="Action to do"
-      role="checkbox"
-      aria-checked="false"
-    />
-  </FormTaskAddStyled>
-)
 
 export default FormTaskAdd
